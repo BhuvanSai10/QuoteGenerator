c71cf81c66c0ae6e8bafc9072dd8da543b65b653
Bhuvan sai <bhuvansai012345@gmail.com>
2023-07-05 08:01:57 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const quote = document.getElementById("quote")
+const author = document.getElementById("author")
+const api_url = "https://api.quotable.io/random"
+
+async function getQuote(url){
+    const response = await fetch(url)
+    let data = await response.json()
+    quote.innerHTML = data.content
+    author.innerHTML = data.author
+}
+
+
+getQuote(api_url)
+
+function tweet(){
+    window.open("https://twitter.com/intent/tweet?text="+quote.innerHTML+"----by "+author.innerHTML,"Tweet Window","Width = 600,height = 300");
+}
